c721e8344c57dbb976172c4ec45af7f2cd60bc3a
Wendel Tonzar <wendeltonzar@gmail.com>
2023-06-27 22:15:40 -0300

    Chatzap created.

---
+++ src/main/java/com/lednewcodes/chatzap/ChatzapApplication.java
@@ -0,0 +1,13 @@
+package com.lednewcodes.chatzap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatzapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatzapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lednewcodes/chatzap/ChatzapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lednewcodes.chatzap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatzapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
